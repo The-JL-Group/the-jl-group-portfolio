98b4dd83267e0c7a9e1c0409d3fd95754e9c1257
jimbolikesgithub <jamesedwards@Jamess-MacBook-Pro.local>
2022-08-03 22:07:07 -0400

    Added the initial global stylings to index css and header css

---
+++ src/components/Header/navIcons.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+// Icons
+import portfolioDark from '../../assets/icons/portfolio-icon-dark.svg'
+import portfolioLight from '../../assets/icons/portfolio-icon-light.svg'
+
+import aboutDark from '../../assets/icons/about-us-icon-dark.svg'
+import aboutLight from '../../assets/icons/about-us-icon-light.svg'
+
+import hireDark from '../../assets/icons/hire-us-icon-dark.svg'
+import hireLight from '../../assets/icons/hire-us-icon-light.svg'
+
+import socialsDark from '../../assets/icons/socials-icon-dark.svg'
+import socialsLight from '../../assets/icons/socials-icon-light.svg'
+
+import githubDark from '../../assets/icons/github-icon-dark.svg'
+import githubLight from '../../assets/icons/github-icon-light.svg'
+
+import fiverrDark from '../../assets/icons/fiverr-icon-dark.svg'
+import fiverrLight from '../../assets/icons/fiverr-icon-light.svg'
+
+import emailDark from '../../assets/icons/fiverr-icon-dark.svg'
+import emailLight from '../../assets/icons/fiverr-icon-light.svg'
+
+import closeDark from '../../assets/icons/x-icon-dark.svg'
+import closeLight from '../../assets/icons/x-icon-light.svg'
+
+import eyeIcon from '../../assets/icons/eye-icon.svg'
+
+  // Regular Icons
+  export const regIcons = [
+    { title: 'PORTFOLIO', src: portfolioDark ,  pathway: '#' },
+    { title: 'ABOUT US', pathway: '#' },
+    { title: 'HIRE US', pathway: '#' },
+    { title: 'SOCIALS', pathway: '#' },
+  ]
+
+  // Social Icons
+  export const socialsIcons = [
+    { title: 'GITHUB', pathway: '#' },
+    { title: 'FIVERR',  pathway: '#' },
+    { title: 'EMAIL US', pathway: '#' },
+    { title: 'CLOSE', pathway: '#' },
+  ]
+
+  // Call-To-Action Icons
+  export  const ctaIcon = [
+    { title: 'View Work', pathway: '#' }
+  ]
+
+
